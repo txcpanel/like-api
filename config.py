96a2fd423e5d4b331975690dbbee31bb0d9225ec
txcpanel <toxicfreepanel@gmail.com>
2025-10-03 07:36:31 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,26 @@
+URLS_LIKE = {
+    "IND": "https://client.ind.freefiremobile.com/LikeProfile",
+    "BR": "https://client.us.freefiremobile.com/LikeProfile",
+    "US": "https://client.us.freefiremobile.com/LikeProfile",
+    "SAC": "https://client.us.freefiremobile.com/LikeProfile",
+    "NA": "https://client.us.freefiremobile.com/LikeProfile",
+    "BD": "https://clientbp.ggblueshark.com/LikeProfile",   # Added BD
+}
 
+URLS_INFO = {
+    "IND": "https://client.ind.freefiremobile.com/GetPlayerPersonalShow",
+    "BR": "https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+    "US": "https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+    "SAC": "https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+    "NA": "https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+    "BD": "https://clientbp.ggblueshark.com/GetPlayerPersonalShow",  # Added BD
+}
 
-URLS_LIKE = {"IND":"https://client.ind.freefiremobile.com/LikeProfile",
-            "BR":"https://client.us.freefiremobile.com/LikeProfile",
-            "US":"https://client.us.freefiremobile.com/LikeProfile",
-            "SAC":"https://client.us.freefiremobile.com/LikeProfile",
-            "NA":"https://client.us.freefiremobile.com/LikeProfile"}
-
-
-URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow",
-            "BR":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
-            "US":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
-            "SAC":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
-            "NA":"https://client.us.freefiremobile.com/GetPlayerPersonalShow"}
-
-
-
-FILES = {"IND":"token_ind.json",
-             "BR":"token_br.json",
-             "US":"token_br.json",
-             "SAC":"token_br.json",
-             "NA":"token_br.json"}
+FILES = {
+    "IND": "token_ind.json",
+    "BR": "token_br.json",
+    "US": "token_br.json",
+    "SAC": "token_br.json",
+    "NA": "token_br.json",
+    "BD": "token_bd.json",   # Added BD
+}
